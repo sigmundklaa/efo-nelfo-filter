598cf3727e36c4048e514da8e5f4c39119977253
sigjig <sigmundklaa@outlook.com>
2023-07-14 23:35:13 +0200

    Add filter.py

---
+++ filter.py
@@ -0,0 +1,49 @@
+
+import csv
+from pathlib import Path
+
+
+ENCODING = 'ISO-8859-1'
+CSV_ARGS = {'delimiter': ';', 'quoting': csv.QUOTE_NONE, 'quotechar': None}
+
+
+def main(inp: Path, out: Path, logfile: Path) -> None:
+    with open(inp, encoding=ENCODING) as ifp, \
+            open(out, 'w', encoding=ENCODING) as ofp, \
+            open(logfile, 'w', encoding=ENCODING) as fp:
+
+        reader = csv.reader(ifp, **CSV_ARGS)
+        writer = csv.writer(ofp, **CSV_ARGS)
+        logger = csv.writer(fp, **CSV_ARGS)
+
+        nvl_add = nvl_del = 0
+
+        for idx, line in enumerate(reader):
+            is_vl = line[0].upper() == 'VL'
+
+            if not is_vl or int(line[8]) > 0:
+                if is_vl:
+                    nvl_add += 1
+                writer.writerow(line)
+            else:
+                if is_vl:
+                    nvl_del += 1
+                logger.writerow([idx+1] + line)
+
+        print(f'VL Added: {nvl_add}, VL Removed {nvl_del}')
+
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str, help='File to read')
+    parser.add_argument('-o', '--output', type=str, default='./out.csv',
+                        help='Path to output')
+    parser.add_argument('-l', '--logfile', type=str,
+                        default='./log.csv', help='Path to logfile')
+
+    opts = parser.parse_args(sys.argv[1:])
+
+    main(inp=Path(opts.file), out=Path(opts.output), logfile=Path(opts.logfile))
